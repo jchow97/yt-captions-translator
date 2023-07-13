cadb451c42bb67f9b217b37cd0a7ccd1fdf91809
Jeffrey Chow <72580584+jchow97@users.noreply.github.com>
2023-07-12 17:19:21 -0700

    Installed tailwind css for styling.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+      "./popup/**/*.{html,js}",
+      "./scripts/**/*.{html,js}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
