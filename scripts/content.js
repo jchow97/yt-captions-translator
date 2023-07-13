395159ed7672573914c5f2169a270c474e15a78e
Jeffrey Chow <72580584+jchow97@users.noreply.github.com>
2023-07-13 02:41:12 -0700

    Added MutationObserver to check for changes to video captions.

--- scripts/content.js
+++ scripts/content.js
@@ -7,5 +7,38 @@ window.onload = function () {
         translateButton.className = 'bg-blue-500 hover:bg-blue-700 text-lg text-white font-sans font-bold py-4 px-6 rounded-full ml-3';
 
         menu.appendChild(translateButton);
+
+        const captionWindowElement = document.querySelector('.ytp-caption-window-container');
+
+        if (captionWindowElement) {
+            const observer = new MutationObserver((mutationsList, observer) => {
+                for (const mutation of mutationsList) {
+                    const targetElement = document.querySelector('span.ytp-caption-segment');
+                    if (mutation.type === 'childList' && mutation.target === targetElement) {
+                        // Retrieve Text
+                        // Replace Text
+                        console.log("Captions Changed!");
+                        break;
+                    }
+                }
+            })
+            const observerConfig = { childList: true, subtree: true };
+            observer.observe(captionWindowElement, observerConfig);
+        }
+
     }, 1000)
+}
+
+function setupMutationObserver(targetElement) {
+    const observer = new MutationObserver(mutationsList => {
+        for (const mutation in mutationsList) {
+            if (mutation.type === '') {
+
+            }
+        }
+    })
+
+    const observerConfig = { childList: true, subtree: true };
+    observer.observe(targetElement, observerConfig);
+
 }
