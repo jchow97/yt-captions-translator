443b504cd050bf9101c8ec531018cda08832baa5
Jeffrey Chow <72580584+jchow97@users.noreply.github.com>
2023-07-18 23:10:34 -0700

    Implemented messaging between service worker and content script, along with caption injection onto site.

--- background.js
+++ background.js
@@ -1,19 +1,25 @@
-let requestCounter = 0
-let translatedTimedText = []
+let requestCounter = 0;
+let translatedTimedText = [];
 chrome.webRequest.onCompleted.addListener(
     async function (details) {
         if (details && requestCounter % 2 === 0) {
+            // Clear existing array
+            translatedTimedText = []
             console.log("Loading: ", details.url);
+
             const response = await fetch(details.url);
             let json = await response.json();
+
             for (let i = 0; i < json.events.length; i++) {
-                const caption = json.events[i].segs[0];
+                const caption = json.events[i].segs[0].utf8;
                 const timeStart = json.events[i].tStartMs;
+                const duration = json.events[i].dDurationMs;
                 translatedTimedText.push(
                     {
                         // TODO: TRANSLATE AND RETURN AN ARRAY OF TRANSLATED STRINGS
                         translatedCaption: "[Translated] " + caption,
-                        timeStart: timeStart
+                        timeStart: timeStart,
+                        duration: duration
                     });
             }
             console.log(json.events);
@@ -22,5 +28,21 @@ chrome.webRequest.onCompleted.addListener(
     },
     {
         urls: ["*://*.youtube.com/api/timedtext*"],
+    }
+)
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        if (request.time) {
+            const requestMs = request.time * 1000;
+            for (let i = 0; i < translatedTimedText.length; i++) {
+                const startTime = translatedTimedText[i].timeStart;
+                const endTime = translatedTimedText[i].timeStart + translatedTimedText[i].duration;
+                if (requestMs >= startTime && requestMs < endTime) {
+                    sendResponse({caption: translatedTimedText[i].translatedCaption})
+                    break;
+                }
+            }
+        }
     }
 )
--- scripts/content.js
+++ scripts/content.js
@@ -1,30 +1,37 @@
-window.onload = function () {
-    let captionsCounter = 0;
-    window.setTimeout(function () {
-        let menu = document.getElementById("center");
-
-        let translateButton = document.createElement('button');
-        translateButton.textContent = 'Translate';
-        translateButton.className = 'bg-blue-500 hover:bg-blue-700 text-lg text-white font-sans font-bold py-4 px-6 rounded-full ml-3';
-
-        menu.appendChild(translateButton);
-
+// window.onload = function () {
+//     window.setTimeout(function () {
+        // let menu = document.getElementById("center");
+        //
+        // let translateButton = document.createElement('button');
+        // translateButton.textContent = 'Translate';
+        // translateButton.className = 'bg-blue-500 hover:bg-blue-700 text-lg text-white font-sans font-bold py-4 px-6 rounded-full ml-3';
+        //
+        // menu.appendChild(translateButton);
         const captionWindowElement = document.querySelector('.ytp-caption-window-container');
 
         if (captionWindowElement) {
-            const observer = new MutationObserver((mutationsList, observer) => {
+            const observer = new MutationObserver(async (mutationsList, observer) => {
                 for (const mutation of mutationsList) {
                     const targetElement = document.querySelector('span.ytp-caption-segment');
                     if (mutation.type === 'childList' && mutation.target === targetElement) {
+                        observer.disconnect();
                         // Retrieve Text
+                        const currentTime = document.querySelector("video").currentTime;
+                        const response = await chrome.runtime.sendMessage({time: currentTime});
                         // Replace Text
-                        console.log("Captions Changed!");
+                        if (response) {
+                            targetElement.textContent = response.caption;
+                        }
+                        console.log("Captions Changed! ", currentTime);
+                        observer.observe(captionWindowElement, observerConfig);
                         break;
                     }
                 }
             })
             const observerConfig = { childList: true, subtree: true };
             observer.observe(captionWindowElement, observerConfig);
+        } else {
+            captionChangeCounter++;
         }
-    }, 1000)
-}
+//     }, 1000)
+// }
