0970f6838447b4271df48dc15533af72648d3824
Jeffrey Chow <72580584+jchow97@users.noreply.github.com>
2023-07-22 10:27:03 -0700

    Removed unused code.

--- scripts/content.js
+++ scripts/content.js
@@ -1,39 +1,28 @@
-// window.onload = function () {
-//     window.setTimeout(function () {
-        // let menu = document.getElementById("center");
-        //
-        // let translateButton = document.createElement('button');
-        // translateButton.textContent = 'Translate';
-        // translateButton.className = 'bg-blue-500 hover:bg-blue-700 text-lg text-white font-sans font-bold py-4 px-6 rounded-full ml-3';
-        //
-        // menu.appendChild(translateButton);
-        const captionWindowElement = document.querySelector('.ytp-caption-window-container');
-        if (captionWindowElement) {
-            const observer = new MutationObserver(async (mutationsList, observer) => {
-                for (const mutation of mutationsList) {
-                    const targetElement = document.querySelector('span.ytp-caption-segment');
-                    if (mutation.type === 'childList' && mutation.target === targetElement) {
-                        observer.disconnect();
-                        // Retrieve Text
-                        const currentTime = document.querySelector("video").currentTime;
-                        const videoId = document.querySelector("video").id;
-                        const response = await chrome.runtime.sendMessage(
-                            {
-                                videoId: videoId,
-                                time: currentTime
-                            });
-                        // Replace Text
-                        if (response) {
-                            targetElement.textContent = response.caption;
-                        }
-                        console.log("Captions Changed! ", currentTime);
-                        observer.observe(captionWindowElement, observerConfig);
-                        break;
-                    }
+const captionWindowElement = document.querySelector('.ytp-caption-window-container');
+if (captionWindowElement) {
+    const observer = new MutationObserver(async (mutationsList, observer) => {
+        for (const mutation of mutationsList) {
+            const targetElement = document.querySelector('span.ytp-caption-segment');
+            if (mutation.type === 'childList' && mutation.target === targetElement) {
+                observer.disconnect();
+                // Retrieve Text
+                const currentTime = document.querySelector("video").currentTime;
+                const videoId = document.querySelector("video").id;
+                const response = await chrome.runtime.sendMessage(
+                    {
+                        videoId: videoId,
+                        time: currentTime
+                    });
+                // Replace Text
+                if (response) {
+                    targetElement.textContent = response.caption;
                 }
-            })
-            const observerConfig = { childList: true, subtree: true };
-            observer.observe(captionWindowElement, observerConfig);
+                console.log("Captions Changed! ", currentTime);
+                observer.observe(captionWindowElement, observerConfig);
+                break;
+            }
         }
-//     }, 1000)
-// }
+    })
+    const observerConfig = { childList: true, subtree: true };
+    observer.observe(captionWindowElement, observerConfig);
+}
