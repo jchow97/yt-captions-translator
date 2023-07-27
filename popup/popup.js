19583cb88a4e10e8a0b1ffea028ac82bcb3aa1ab
Jeffrey Chow <72580584+jchow97@users.noreply.github.com>
2023-07-26 20:43:16 -0700

    Uses chrome.storage for settings persistence. Implements load and reload for popup.

--- popup/popup.js
+++ popup/popup.js
@@ -1,17 +1,47 @@
-let settings = {
-    translate: false,
-    language: "",
-    engine: ""
+// Get page elements
+const translateCheckbox = document.querySelector('input[type="checkbox"]');
+const languageDropdown = document.getElementById('language-label');
+const engineDropdown = document.getElementById('engine-label');
+
+// Saves options to chrome.storage
+const saveOptions = () => {
+    chrome.storage.local.set(
+        {
+            translate: translateCheckbox.checked,
+            languageName: languageDropdown.innerText,
+            languageCode: languageDropdown.getAttribute('data-value'),
+            engineName: engineDropdown.innerText,
+            engineCode: engineDropdown.getAttribute('data-value')
+        }
+    );
 }
-chrome.storage.local.set(settings);
 
-var translateCheckbox = document.querySelector('input[type="checkbox"]');
+// Restores toggle and dropdown state using the settings stored in chrome.storage.
+const restoreOptions = () => {
+    debugger;
+    chrome.storage.local.get(
+        {
+            translate: 'false',
+            languageName: 'Select a Language',
+            languageCode: '',
+            engineName: 'Select an Engine',
+            engineCode: ''
+        },
+        (items) => {
+            translateCheckbox.checked = items.translate;
+            languageDropdown.innerText = items.languageName;
+            languageDropdown.setAttribute('data-value', items.languageCode);
+            engineDropdown.innerText = items.engineName;
+            engineDropdown.setAttribute('data-value', items.engineCode);
+        }
+    );
+}
 
-translateCheckbox.addEventListener('change', function() {
-    settings.translate = this.checked;
-    chrome.storage.local.set(settings);
-});
+document.addEventListener('DOMContentLoaded', restoreOptions);
 
+translateCheckbox.addEventListener('change', saveOptions);
+
+// On click, display or hide the dropdown.
 var dropdownToggles = document.querySelectorAll('.dropdown-toggle');
 dropdownToggles.forEach(function (toggle) {
     toggle.addEventListener('click', function () {
@@ -24,25 +54,18 @@ dropdownToggles.forEach(function (toggle) {
 var menuItems = document.querySelectorAll('[role="menuitem"]');
 menuItems.forEach(function (menuItem) {
     menuItem.addEventListener('click', function () {
+        // Hide dropdown on menu item click.
         event.preventDefault();  // Prevent navigation
         var dropdownContent = this.parentElement.parentElement;
         dropdownContent.classList.add('hidden');
 
-        // Do something
-        var value = this.getAttribute('data-value');
-        if (this.parentElement.parentElement.id === "language-dropdown-content") {
-            settings.language = value;
-            chrome.storage.local.set(settings);
-            console.log("You clicked language: " + value)
-        } else if (this.parentElement.parentElement.id === "engine-dropdown-content") {
-            settings.engine = value;
-            chrome.storage.local.set(settings);
-            console.log("You clicked engine: " + value)
-        }
-
-        // Update the dropdown button's text
+        // Update the dropdown button's text and data value.
         var dropdownLabel = this.parentElement.parentElement.previousElementSibling.querySelector('.dropdown-label');
         dropdownLabel.innerText = this.innerText;
+        dropdownLabel.setAttribute('data-value', this.getAttribute('data-value'));
+
+        // Save new settings.
+        saveOptions();
     });
 });
 
